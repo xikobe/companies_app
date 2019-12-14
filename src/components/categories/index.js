6261e3d98b8782802be705b044b78c7ad838726e
Francisco Ferreira <fppf@hellofresh.com>
2019-12-14 16:37:45 +0100

    feat: Add categories section

--- src/components/categories/index.js
+++ src/components/categories/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectAppsCategories } from '../../store/apps-list/selectors';
 
-const Categories = () => (
-  <p>Categories</p>
-);
+const Categories = () => {
+  const categories = useSelector(selectAppsCategories);
+
+  console.log(categories);
+
+  return (
+    <p>Categories</p>
+  );
+};
 
 export default Categories;
